2161559effdfa3867cab15d1e7cde1cf6befcf9d
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-09-24 20:28:20 -0700

    Attempt at turning the patients table into a reusable component

--- frontend/frontend/src/Pages/ViewOfPatients.js
+++ frontend/frontend/src/Pages/ViewOfPatients.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import TableComponent from "../Components/TableComponent";
 import { DataGrid } from "@mui/x-data-grid";
 // import Paper from "@mui/material/Paper";
 import {
@@ -17,15 +17,15 @@ function ViewOfPatients() {
 		{ field: "id", headerName: "ID", width: 70 },
 		{ field: "firstName", headerName: "First name", width: 130 },
 		{ field: "lastName", headerName: "Last name", width: 130 },
-		{
-			field: "fullName",
-			headerName: "Full name",
-			description: "This column has a value getter and is not sortable.",
-			sortable: false,
-			width: 160,
-			valueGetter: (value, row) =>
-				`${row.firstName || ""} ${row.lastName || ""}`,
-		},
+		// {
+		// 	field: "fullName",
+		// 	headerName: "Full name",
+		// 	description: "This column has a value getter and is not sortable.",
+		// 	sortable: false,
+		// 	width: 160,
+		// 	valueGetter: (value, row) =>
+		// 		`${row.firstName || ""} ${row.lastName || ""}`,
+		// },
 		{ field: "dateOfBirth", headerName: "Date of Birth", width: 160 },
 
 		{
@@ -377,6 +377,7 @@ function ViewOfPatients() {
 						}}
 					/>
 				</Paper>
+				{/* <TableComponent rows={rows}></TableComponent> */}
 			</div>
 		</div>
 	);

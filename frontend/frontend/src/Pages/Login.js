d5bb4715dedef91651714d2f72da47596e825834
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-09-25 13:18:23 -0700

    Styling on login form

--- frontend/frontend/src/Pages/Login.js
+++ frontend/frontend/src/Pages/Login.js
@@ -6,40 +6,42 @@ function Login() {
 		<div>
 			<h1>Employee Login</h1>
 			<h3>Welcome to the Pharmacy Management System</h3>
-			<div className="centered-div">
-				<div className="form-group">
-					<label htmlFor="email" className="input-label">
-						Email:
-					</label>
-					<input
-						type="text"
-						className="input-field"
-						id="email"
-						placeholder="Email"
-					/>
-				</div>
+			<div className="centered-div-container">
+				<div className="login-box">
+					<div className="form-group">
+						<label htmlFor="email" className="input-label">
+							Email:
+						</label>
+						<input
+							type="text"
+							className="input-field"
+							id="email"
+							placeholder="Email"
+						/>
+					</div>
 
-				<div className="form-group">
-					<label htmlFor="password" className="input-label">
-						Password:
-					</label>
-					<input
-						type="password"
-						className="input-field"
-						id="password"
-						placeholder="Password"
-					/>
-				</div>
+					<div className="form-group">
+						<label htmlFor="password" className="input-label">
+							Password:
+						</label>
+						<input
+							type="password"
+							className="input-field"
+							id="password"
+							placeholder="Password"
+						/>
+					</div>
 
-				<div>
-					<p>
-						Don't have an account yet? Ask your pharmacy manager to
-						create one for you.
-					</p>
-				</div>
+					<div>
+						<p>
+							Don't have an account yet? Ask your pharmacy manager
+							to create one for you.
+						</p>
+					</div>
 
-				<div>
-					<input type="button" id="submit" value="Submit" />
+					<div>
+						<input type="button" id="submit-button" value="Submit" />
+					</div>
 				</div>
 			</div>
 		</div>

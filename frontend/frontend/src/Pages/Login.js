54db831be63eccbcc2e1d16974755b9bc833a3fc
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-09-24 17:05:52 -0700

    Login page with stylesheet for it

--- frontend/frontend/src/Pages/Login.js
+++ frontend/frontend/src/Pages/Login.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import "../Styles/Login.css"; // import the specific stylesheet for this page
 
 function Login() {
-  return (
-    <div>
-      <p> login page </p>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Employee Login</h1>
+			<h3>Welcome to the Pharmacy Management System</h3>
+			<div className="centered-div">
+				<div className="form-group">
+					<label htmlFor="email" className="input-label">
+						Email:
+					</label>
+					<input
+						type="text"
+						className="input-field"
+						id="email"
+						placeholder="Email"
+					/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="password" className="input-label">
+						Password:
+					</label>
+					<input
+						type="password"
+						className="input-field"
+						id="password"
+						placeholder="Password"
+					/>
+				</div>
+
+				<div>
+					<p>
+						Don't have an account yet? Ask your pharmacy manager to
+						create one for you.
+					</p>
+				</div>
+
+				<div>
+					<input type="button" id="submit" value="Submit" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;

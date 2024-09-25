b91f6d0c4a3c522dd5d06532a6d65dbd07c89598
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-09-24 17:05:33 -0700

    Added a global stylesheet, importing it in index.js

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './Styles/styles.css'; // import the global stylesheet so it gets used on all pages
 import './Styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

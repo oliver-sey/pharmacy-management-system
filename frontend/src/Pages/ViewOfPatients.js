5dfe4e4031e6da53c20e3af60f53a92243ff9347
Skyler DeVaughn <skylerjoe@email.arizona.edu>
2024-10-16 11:16:08 -0700

    server side input validation

--- frontend/src/Pages/ViewOfPatients.js
+++ frontend/src/Pages/ViewOfPatients.js
@@ -1,15 +1,17 @@
 import { React, useRef, useState, useEffect } from "react";
+import { Snackbar, Alert, Button} from "@mui/material";
 
 import EditDeleteTable from "../Components/EditDeleteTable";
 import AddEditPatientModal from "../Components/AddEditPatientModal";
 import DeleteModal from "../Components/DeleteModal";
-import Button from "@mui/material/Button";
 
 function ViewOfPatients() {
 	// the columns for the table
 	// headerName is what shows up on the website
 	// width is the default width of the column, user can adjust it
 	const [rows, setRows] = useState([]);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [openSnackbar, setOpenSnackbar] = useState(false);
 
 	// Async function to fetch patients data
 	const fetchPatients = async () => {
@@ -19,6 +21,9 @@ function ViewOfPatients() {
 		  setRows(data); // Update rows state with fetched data
 		} catch (error) {
 		  console.error('Error fetching patients:', error);
+		  // error handling
+		  setErrorMessage('Failed to fetch patients');
+      	  setOpenSnackbar(true); // Show Snackbar when error occurs
 		}
 	}; 
 
@@ -65,6 +70,8 @@ function ViewOfPatients() {
 			fetchPatients();
 		} catch (error) {
 			console.error('Error deleting patient:', error);
+			setErrorMessage('Failed to delete patient' + error);
+			setOpenSnackbar(true);
 		}
 	}
 
@@ -78,7 +85,6 @@ function ViewOfPatients() {
 
 	const editPatient = async (data, id) => {
 		try {
-			
 			console.log("row in editPatient", id, data)
 			const response = await fetch(`http://localhost:8000/patient/${id}`, {
 				method: 'PUT',
@@ -88,11 +94,14 @@ function ViewOfPatients() {
 				body: JSON.stringify(data),
 			});
 			if (!response.ok) {
-				throw new Error('Failed to update patient');
+				const responseData = await response.json(); // Wait for the JSON to be parsed
+				console.log("Error detail from response:", responseData.detail[0].msg);
+				throw new Error(responseData.detail[0].msg);
 			}
 			fetchPatients();
 		} catch (error) {
-			console.error('Error updating patient:', error);
+			setErrorMessage('Failed to update patient');
+			setOpenSnackbar(true);
 		}
 	}
 
@@ -107,13 +116,27 @@ function ViewOfPatients() {
 				body: JSON.stringify(data),
 			});
 			if (!response.ok) {
-				throw new Error('Failed to add patient');
+				const responseData = await response.json(); // Wait for the JSON to be parsed
+				var errorMessage;
+				// if responseData.detail is a string, return the strig, else return the first element of the array
+				if (typeof(responseData.detail) == 'string') {// check if response.detail is a string or an array
+					errorMessage = responseData.detail;
+				} else {
+					errorMessage = responseData.detail[0].msg;
+				}
+				throw new Error(errorMessage);
 			}
 			fetchPatients();
 		} catch (error) {
-			console.error('Error adding patient:', error);
+			setErrorMessage('Failed to add patient: ' + error);
+			setOpenSnackbar(true);
 		}
 	}
+
+	// Handle closing of the Snackbar
+	const handleCloseSnackbar = () => {
+		setOpenSnackbar(false);
+	};
 
 	// the message format that should get used in the delete confirmation modal (popup) for this table
 	// need this since we want a different format on other tables that use this same base component
@@ -152,6 +175,16 @@ function ViewOfPatients() {
 			onEdit={addEditPatient}
 			onConfirmDelete={deletePatient}
 		  />
+		  {/* Snackbar for error messages */}
+		  <Snackbar 
+			open={openSnackbar} 
+			autoHideDuration={6000} 
+			onClose={handleCloseSnackbar}
+		  >
+			<Alert onClose={handleCloseSnackbar} severity="error">
+			{errorMessage}
+			</Alert>
+		  </Snackbar>
 		</div>
 	  );
 	  

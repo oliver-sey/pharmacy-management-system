66a66c3476f26c9d6161750d5a3293b995f739f6
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-11-27 13:36:47 -0700

    Making more realistic medication prices in the canned data

--- backend/database/canned data.sql
+++ backend/database/canned data.sql
@@ -95,47 +95,47 @@ INSERT INTO patients(first_name, last_name, date_of_birth, address, phone_number
 -- medications table
 
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Ibuprofen', '200 mg', 500, FALSE, '2025-05-10', 0.1);
+VALUES('Ibuprofen', '200 mg', 500, FALSE, '2025-05-10', 0.15); -- id #1
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Amoxicillin', '500 mg', 200, TRUE, '2024-12-01', 2);
+VALUES('Amoxicillin', '500 mg', 200, TRUE, '2024-12-01', 0.3); -- id #2
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Lipitor', '20 mg', 300, TRUE, '2026-03-15', 0.75);
+VALUES('Lipitor', '20 mg', 300, TRUE, '2026-03-15', 0.5); -- id #3
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Acetaminophen', '500 mg', 1000, FALSE, '2025-11-30', 0.08);
+VALUES('Acetaminophen', '500 mg', 1000, FALSE, '2025-11-30', 0.1); -- id #4
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Aspirin', '100 mg', 500, FALSE, '2024-07-22', 0.05);
+VALUES('Aspirin', '100 mg', 500, FALSE, '2024-07-22', 0.09); -- id #5
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Warfarin', '2.5 grams', 150, TRUE, '2024-10-10', 1.5);
+VALUES('Warfarin', '2.5 grams', 150, TRUE, '2024-10-10', 0.6); -- id #6
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Metformin', '850 mg', 400, TRUE, '2025-02-14', 0.5);
+VALUES('Metformin', '850 mg', 400, TRUE, '2025-02-14', 0.03); -- id #7
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Melatonin', '20mL', 5, FALSE, '2024-10-31', 0.0004);
+VALUES('Melatonin', '20mL', 5, FALSE, '2024-10-31', 0.08); -- id #8
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Dramamine', '1000 mg', 1200, FALSE, '2021-10-01', 0.51);
+VALUES('Dramamine', '1000 mg', 1200, FALSE, '2021-10-01', 0.07); -- id #9
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Ibuprofen', '200 mg', 500, FALSE, '2025-05-10', 0.1);
+VALUES('Ibuprofen', '200 mg', 500, FALSE, '2025-05-10', 0.09); -- id #10
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Amoxicillin', '500 mg', 100, TRUE, '2024-12-01', 2);
+VALUES('Amoxicillin', '600 mg', 100, TRUE, '2024-12-01', 0.2); -- id #11
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Lipitor', '20 mg', 300, TRUE, '2026-03-15', 0.75);
+VALUES('Lipitor', '20 mg', 300, TRUE, '2026-03-15', 0.1); -- id #12
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Acetaminophen', '500 mg', 1000, FALSE, '2025-11-30', 0.08);
+VALUES('Acetaminophen', '1000 mg', 1000, FALSE, '2025-11-30', 0.20); -- id #13
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Aspirin', '100 nanograms', 200, FALSE, '2024-07-22', 0.05);
+VALUES('Aspirin', '100 nanograms', 200, FALSE, '2024-07-22', 0.05); -- id #14
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Hydrochlorothiazide', '12.5 mg', 600, TRUE, '2026-02-18', 0.5);
+VALUES('Hydrochlorothiazide', '12.5 mg', 600, TRUE, '2026-02-18', 0.30); -- id #15
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Simvastatin', '40 mg', 1, TRUE, '2025-09-20', 1.1);
+VALUES('Simvastatin', '40 mg', 1, TRUE, '2025-09-20', 0.2); -- id #16
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Metformin', '1000 mg', 400, TRUE, '2026-05-23', 0.25);
+VALUES('Metformin', '1000 mg', 400, TRUE, '2026-05-23', 0.03); -- id #17
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Prednisone', '10 mg', 500, TRUE, '2024-10-05', 1.5);
+VALUES('Prednisone', '10 mg', 500, TRUE, '2024-10-05', 0.2); -- id #18
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Warfarin', '5g', 100, TRUE, '2025-04-20', 0.8);
+VALUES('Warfarin', '5g', 100, TRUE, '2025-04-20', 0.06); -- id #19
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Losartan', '50 grams', 200, TRUE, '2024-11-11', 1);
+VALUES('Losartan', '50 grams', 200, TRUE, '2024-11-11', 0.03); -- id #20
 INSERT INTO medications(name, dosage, quantity, prescription_required, expiration_date, dollars_per_unit) 
-VALUES('Levothyroxine', '75 mg', 300, TRUE, '2026-01-01', 0.55);
+VALUES('Levothyroxine', '75 mg', 300, TRUE, '2026-01-01', 0.04); -- id #21
 
 
 

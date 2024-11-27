1cb0f5796ab3869210e26fa24ae9fe6053dff950
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-11-27 11:32:05 -0700

    Formatting handleSubmit() in Login

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -116,7 +116,7 @@ function Login({ updateUserRole }) {
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 
-    // ensures form is filled
+		// ensures form is filled
 		if (!validateForm()) return;
 		setLoading(true);
 
@@ -134,64 +134,65 @@ function Login({ updateUserRole }) {
 				body: formDetails,
 			});
 
-      setLoading(false);
-      
-      
-      if (response.ok) {
-        // store response token when response is received
-        const data = await response.json();
+			setLoading(false);
 
-        // If login is successful, clear failed attempts for this email address
+			if (response.ok) {
+				// store response token when response is received
+				const data = await response.json();
+
+				// If login is successful, clear failed attempts for this email address
 				resetFailedAttempts(username);
 
-        localStorage.setItem('token', data.access_token);
-        console.log(localStorage.getItem('token'))
+				localStorage.setItem("token", data.access_token);
+				console.log(localStorage.getItem("token"));
 
-        //fetches information about current user
-        const userResponse = await fetch('http://localhost:8000/currentuser/me', {
-          method: 'GET',
-          headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+				//fetches information about current user
+				const userResponse = await fetch(
+					"http://localhost:8000/currentuser/me",
+					{
+						method: "GET",
+						headers: {
+							Authorization:
+								"Bearer " + localStorage.getItem("token"),
+						},
+					}
+				);
 
-        })
+				if (userResponse.ok) {
+					//stores user info
+					//TO DO: put user info in local storage?
+					const userData = await userResponse.json();
+					localStorage.setItem("role", userData.user_type);
 
-        if (userResponse.ok) {
-          //stores user info
-          //TO DO: put user info in local storage?
-          const userData = await userResponse.json();
-          localStorage.setItem('role', userData.user_type)
-		  
+					// Update the user role immediately
+					updateUserRole(userData.user_type); // update the header
+					console.log("User role:", userData.user_type); // Log the user role
 
-		  // Update the user role immediately
-		  updateUserRole(userData.user_type); // update the header
-          console.log("User role:", userData.user_type); // Log the user role
+					//redirects user to homepage for their role
+					//more can be added as needed
+					if (userData.user_type === "Pharmacy Manager") {
+						navigate("../managerhome", { replace: true });
+					} else if (userData.user_type === "Pharmacist") {
+						navigate("../pharmacisthome", { replace: true });
+					} else if (userData.user_type === "Cashier") {
+						navigate("../cashierhome", { replace: true });
+					} else if (userData.user_type === "Pharmacy Technician") {
+						navigate("../pharmtechhome", { replace: true });
+					} else {
+						navigate("../protected", { replace: true });
+					}
+				}
+			} else {
+				const errorData = await response.json();
+				setError(errorData.detail || "Authentication failed!");
 
-          //redirects user to homepage for their role
-          //more can be added as needed
-          if (userData.user_type === 'Pharmacy Manager') {
-            navigate('../managerhome', {replace: true})
-          } else if (userData.user_type === 'Pharmacist') {
-            navigate('../pharmacisthome', {replace: true})
-          } else if (userData.user_type === 'Cashier'){
-			navigate('../cashierhome', {replace: true})
-		  } else if (userData.user_type === 'Pharmacy Technician'){
-			navigate('../pharmtechhome', {replace: true})
-		  }else {
-            navigate('../protected', {replace: true})
-          }
-      }
-       
-      } else {
-        const errorData = await response.json();
-        setError(errorData.detail || 'Authentication failed!');
-
-        handleIncorrectAttempt(username);
-      }
-
-      } catch (error) {
-        setLoading(false);
-        setError('An error occurred. Please try again later.');
-      }
-    }
+				handleIncorrectAttempt(username);
+			}
+		} catch (error) {
+			setLoading(false);
+			setError("An error occurred. Please try again later.");
+		}
+	};
   
 
   return(
